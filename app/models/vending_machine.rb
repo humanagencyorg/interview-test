84b8e877a19eb60a3753f2a5a9dbdfbaa0e2ecb7
Mike Menne <mike@humanagency.com>
2021-08-18 22:15:51 -0500

    One possible solution to vending machine kata

--- app/models/vending_machine.rb
+++ app/models/vending_machine.rb
@@ -11,4 +11,48 @@
 require "money"
 
 class VendingMachine
+
+  def initialize(inventory = Inventory.new,
+                 coin_dispenser = CoinDispenser.new)
+    @inventory = inventory
+    @coin_dispenser = coin_dispenser
+    @balance = Money.new(0, :usd)
+  end
+
+  def deposit(amount)
+    @balance += Monetize.parse(amount)
+    @message = @balance.format
+  end
+
+  def display
+    @message
+  end
+
+  def purchase
+    if @balance >= @inventory.price
+      @inventory.dispense
+      @message = "Thank you!"
+      if @balance > @inventory.price
+        returned_change = @balance - @inventory.price
+        @coin_dispenser.return_money(returned_change)
+        @balance -= returned_change
+      end
+    else
+      @message = "Please insert more money"
+    end
+  end
+end
+
+class Inventory
+  attr_reader :price
+
+  def initialize(price: nil)
+    @price = price
+  end
+
+  def dispense; end
+end
+
+class CoinDispenser
+  def return_money(amount); end
 end
--- spec/models/vending_machine_spec.rb
+++ spec/models/vending_machine_spec.rb
@@ -2,4 +2,86 @@ require "rails_helper"
 require "money"
 
 RSpec.describe VendingMachine do
+  it "shows the balance of the money inserted" do
+    vend = VendingMachine.new
+
+    vend.deposit("$.25")
+    vend.deposit("$.25")
+    vend.deposit("$.25")
+
+    expect(vend.display).to eq("$0.75")
+  end
+
+  it "allows you to purchase a soda" do
+    inventory_stub = instance_double(Inventory,
+                                     price: Money.new(50, :usd))
+    vend = VendingMachine.new(inventory_stub)
+    allow(inventory_stub).to receive(:dispense)
+
+    vend.deposit("$.25")
+    vend.deposit("$.25")
+    vend.deposit("$.25")
+    vend.purchase
+
+    expect(inventory_stub).to have_received(:dispense)
+  end
+
+  it "should say thank you after you purchase" do
+    inventory_stub = instance_double(Inventory,
+                                     price: Money.new(50, :usd))
+    vend = VendingMachine.new(inventory_stub)
+    allow(inventory_stub).to receive(:dispense)
+
+    vend.deposit("$.25")
+    vend.deposit("$.25")
+    vend.deposit("$.25")
+    vend.purchase
+
+    expect(vend.display).to eq("Thank you!")
+  end
+
+  context "when inadequate funds at purchase" do
+    it "should display an error message" do
+      inventory = Inventory.new(price: Money.new(50, :usd))
+      vend = VendingMachine.new(inventory)
+
+      vend.deposit("$.25")
+      vend.purchase
+
+      expect(vend.display).to eq("Please insert more money")
+    end
+  end
+
+  context "when extra funds exist after purchase" do
+    it "should return the change" do
+      inventory = Inventory.new(price: Money.new(50, :usd))
+      coin_stub = instance_double(CoinDispenser)
+      vend = VendingMachine.new(inventory, coin_stub)
+      allow(coin_stub).to receive(:return_money)
+
+      vend.deposit("$.25")
+      vend.deposit("$.25")
+      vend.deposit("$.25")
+      vend.purchase
+
+      expect(coin_stub).
+        to have_received(:return_money).
+        with(Money.new(25, :usd))
+    end
+
+    it "should reset the balance for the next purchase" do
+      inventory_stub = instance_double(Inventory, price: Money.new(50, :usd))
+      vend = VendingMachine.new(inventory_stub)
+      allow(inventory_stub).to receive(:dispense)
+
+      vend.deposit("$.25")
+      vend.deposit("$.25")
+      vend.deposit("$.25")
+      vend.deposit("$.25")
+      vend.purchase
+      vend.purchase
+
+      expect(inventory_stub).not_to receive(:dispense)
+    end
+  end
 end
